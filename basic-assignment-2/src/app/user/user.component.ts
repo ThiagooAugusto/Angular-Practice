954b5dfa4ec3e5c8f30dfec56fdcc4518ac39881
Thiago Augusto <89883388+ThiagooAugusto@users.noreply.github.com>
2024-07-29 13:35:52 -0300

    Add files via upload

---
+++ basic-assignment-2/src/app/user/user.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  userName = '';
+  allowReset;
+
+  onInput(){
+    this.allowReset = this.userName.length == 0 ? false : true;
+  }
+
+  usernameReset(){
+    this.userName = '';
+    this.allowReset = false;
+  }
+}
